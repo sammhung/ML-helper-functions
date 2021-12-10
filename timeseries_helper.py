843deaa3e7909d8febc2157f86850eaaf8292d1b
sammhung <73333796+sammhung@users.noreply.github.com>
2021-12-10 18:09:43 +1100

    Update timeseries_helper.py

--- timeseries_helper.py
+++ timeseries_helper.py
@@ -53,3 +53,16 @@ def make_windows(x, window_size=7, horizon=1):
   windows, labels = get_labelled_window(windowed_array, horizon=horizon)
 
   return windows, labels
+
+# Make the train/test splits
+def make_train_test_splits(windows, labels, test_split=0.2):
+  """
+  Splits matching pairs of windows and labels into train and test splits.
+  """
+  split_size = int(len(windows) * (1-test_split)) 
+  train_windows = windows[:split_size]
+  train_labels = labels[:split_size]
+  test_windows = windows[split_size:]
+  test_labels = labels[split_size:]
+
+  return train_windows, test_windows, train_labels, test_labels
