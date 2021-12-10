a627c8b54540647e2e9ecf1844b3a156415c97e0
sammhung <73333796+sammhung@users.noreply.github.com>
2021-12-10 18:07:01 +1100

    Add files via upload

---
+++ timeseries_helper.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+
+def plot_time_series(timesteps, values, format=".", start=0, end=None, label=None):
+  """
+  Plot's timesteps (a series of points in time) against values (a series of values across timesteps)
+
+  Parameters
+  ----------
+  timesteps : array of timestep values
+  values : array of values across time
+  format : style of plot, default "."
+  start : where to start the plot (setting a value will index from start of timesteps)
+  end : Where to end the plot (similar to start but for the end)
+  label : label to show on plot about values
+  """
+  # Plot the series
+  #plt.figure(figsize=(10, 7))
+  plt.plot(timesteps[start:end], values[start:end], format, label=label)
+  plt.xlabel("Time")
+  plt.ylabel("BTC Price")
+  if label:
+    plt.legend(fontsize=14)
+  plt.grid(True)
+
+# Create function to label windowed data
+def get_labelled_window (x, horizon=1):
+  """
+  Creates labels for windowed dataset.
+
+  E.g. if horizon=1
+  Input: [0, 1, 2, 3, 4, 5, 6, 7] -> Output: ([0, 1, 2, 3, 4, 5, 6, 7], [8])
+  """
+  return x[:, :-horizon], x[:, -horizon:]
+
+# Create function to view NumPy arrays as windows
+def make_windows(x, window_size=7, horizon=1):
+  """
+  Turns a 1D array into a 2D array of Sequential labelled windows of window_size with horizon size labels.
+  """
+  # 1. Create a window step of specific window size (add the horizon on the end for labelling later)
+  window_step = np.expand_dims(np.arange(window_size+horizon), axis=0)
+
+  # 2. Create a 2D array of multiple window steps (minus 1 to account of 0 indexing)
+  window_indexes = window_step + np.expand_dims(np.arange(len(x) - (window_size + horizon - 1)), axis=0).T # Create 2D array of window
+
+  # 3. Index on the target array (a time series) with 2D array of multiple window steps
+  windowed_array = x[window_indexes]
+
+  # 4. Use get_label_window function
+  windows, labels = get_labelled_window(windowed_array, horizon=horizon)
+
+  return windows, labels
