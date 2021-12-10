f3fbbc85fefb8665ec673b319c68a7417507fa3a
sammhung <73333796+sammhung@users.noreply.github.com>
2021-12-10 21:25:59 +1100

    Update timeseries_helper.py

--- timeseries_helper.py
+++ timeseries_helper.py
@@ -66,3 +66,34 @@ def make_train_test_splits(windows, labels, test_split=0.2):
   test_labels = labels[split_size:]
 
   return train_windows, test_windows, train_labels, test_labels
+
+# MASE implementation
+def mean_absolute_scaled_error(y_true, y_pred):
+  """
+  Implement MASE (assuming no seasonality of data).
+  """
+  mae = tf.reduce_mean(tf.abs(y_true-y_pred))
+
+  # Find MAE of naive forecast (no seasonality)
+  mae_naive_no_season = tf.reduce_mean(tf.abs(y_true[1:] - y_true[:-1])) # Our seasonality is one day (hense the shift of 1)
+
+  return mae / mae_naive_no_season
+
+def evaluate_preds(y_true, y_pred):
+
+  y_true = tf.cast(y_true, dtype=tf.float32)
+  y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+  mae = tf.keras.metrics.mean_absolute_error(y_true, y_pred)
+  mse = tf.keras.metrics.mean_squared_error(y_true, y_pred)
+  rmse = tf.sqrt(mse)
+  mape = tf.keras.metrics.mean_absolute_percentage_error(y_true, y_pred)
+  mase = mean_absolute_scaled_error(y_true, y_pred)
+  
+  return {
+      "mae" : mae.numpy(),
+      "mse" : mse.numpy(),
+      "rmse" : rmse.numpy(),
+      "mape" : mape.numpy(),
+      "mase" : mase.numpy()
+  }
